cb4465d34627066c2285ae8585c15ff0cabf60f7
DonDejvo <dada542cz@gmail.com>
2021-04-03 12:09:52 +0200

    signin test

--- scripts/login.js
+++ scripts/login.js
@@ -4,7 +4,18 @@ function handleSignIn() {
     firebase.auth().signInWithPopup(provider)
     .then(result => {
         console.log(result);
-    }).catch(error => {
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+function handleLogout() {
+    firebase.auth().signOut()
+    .then(() => {
+        console.log("Logout successfully");
+    })
+    .catch(error => {
         console.log(error);
     });
 }
