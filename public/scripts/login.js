fe911c73996575780d9d9534d63ed8fbd6e2993e
DonDejvo <dada542cz@gmail.com>
2021-04-03 12:32:59 +0200

    firebase deploy

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,13 +1,7 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 
 function handleSignIn() {
-    firebase.auth().signInWithPopup(provider)
-    .then(result => {
-        console.log(result);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+    firebase.auth().signInWithRedirect(provider);
 }
 
 function handleLogout() {
